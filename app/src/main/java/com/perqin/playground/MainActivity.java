ae99c215c49fff000e0bca1aa2a01f09be13698d
perqin <perqinxie@gmail.com>
2017-03-18 00:14:08 +0800

    Tested changing layout params

--- app/src/main/java/com/perqin/playground/MainActivity.java
+++ app/src/main/java/com/perqin/playground/MainActivity.java
@@ -2,58 +2,62 @@ package com.perqin.playground;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
+public class MainActivity extends AppCompatActivity /*implements GestureDetector.OnGestureListener*/ {
     private static final String TAG = "MainActivity";
-    private GestureDetector detector;
+    private View mBox;
+//    private GestureDetector detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        detector = new GestureDetector(this, this);
+        mBox = findViewById(R.id.box);
+//        detector = new GestureDetector(this, this);
     }
 
-//    public void mainClick(View view) {
-//        startActivity(new Intent(this, SecondActivity.class));
+    public void mainClick(View view) {
+        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) mBox.getLayoutParams();
+        params.topMargin += 150;
+        params.height += 150;
+        mBox.setLayoutParams(params);
+    }
+
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//        Log.i(TAG, "onTouchEvent");
+//        return detector.onTouchEvent(event);
 //    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        Log.i(TAG, "onTouchEvent");
-        return detector.onTouchEvent(event);
-    }
-
-    @Override
-    public boolean onDown(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public void onShowPress(MotionEvent e) {
-    }
-
-    @Override
-    public boolean onSingleTapUp(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        Log.i(TAG, "onScroll");
-        return false;
-    }
-
-    @Override
-    public void onLongPress(MotionEvent e) {
-    }
-
-    @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        Log.i(TAG, "onFling");
-        return false;
-    }
+//
+//    @Override
+//    public boolean onDown(MotionEvent e) {
+//        return false;
+//    }
+//
+//    @Override
+//    public void onShowPress(MotionEvent e) {
+//    }
+//
+//    @Override
+//    public boolean onSingleTapUp(MotionEvent e) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+//        Log.i(TAG, "onScroll");
+//        return false;
+//    }
+//
+//    @Override
+//    public void onLongPress(MotionEvent e) {
+//    }
+//
+//    @Override
+//    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+//        Log.i(TAG, "onFling");
+//        return false;
+//    }
 }
