c214fd29be0d8e2ed19a67530eb9113201be6f70
perqin <perqinxie@gmail.com>
2017-03-17 23:08:37 +0800

    Initial commit

---
+++ app/src/main/java/com/perqin/playground/FlingView.java
@@ -0,0 +1,70 @@
+package com.perqin.playground;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Author   : perqin
+ * Date     : 17-3-17
+ */
+
+public class FlingView extends View implements GestureDetector.OnGestureListener {
+    private static final String TAG = "FlingView";
+
+    private GestureDetector detector;
+
+    public FlingView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        initDetector(context);
+    }
+
+    public FlingView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initDetector(context);
+    }
+
+    private void initDetector(Context context) {
+        detector = new GestureDetector(context, this);
+        detector.setIsLongpressEnabled(false);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return detector.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        Log.i(TAG, "onScroll");
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        Log.i(TAG, "onFling");
+        return false;
+    }
+}
---
+++ app/src/main/java/com/perqin/playground/MainActivity.java
@@ -0,0 +1,60 @@
+package com.perqin.playground;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
+    private static final String TAG = "MainActivity";
+    private GestureDetector detector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        detector = new GestureDetector(this, this);
+    }
+
+    public void mainClick(View view) {
+        startActivity(new Intent(this, SecondActivity.class));
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return detector.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        Log.i(TAG, "onScroll");
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        Log.i(TAG, "onFling");
+        return false;
+    }
+}
