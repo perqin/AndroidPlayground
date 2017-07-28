fc8036506cebfcb12926a3438ea4970c74ae7d23
perqin <perqinxie@gmail.com>
2017-07-28 17:35:36 +0800

    Test tinting

--- app/src/main/java/com/perqin/playground/indicatortint/IndicatorTintActivity.java
+++ app/src/main/java/com/perqin/playground/indicatortint/IndicatorTintActivity.java
@@ -1,13 +1,22 @@
 package com.perqin.playground.indicatortint;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.perqin.playground.R;
 
 public class IndicatorTintActivity extends AppCompatActivity {
+    private static final String TAG = "IndicatorTintActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +32,22 @@ public class IndicatorTintActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.action_indicator_tint, menu);
+        for (int i = 0; i < menu.size(); ++i) {
+            MenuItem item = menu.getItem(i);
+            DrawableCompat.setTint(item.getIcon(), getActionIconColor());
+        }
         return true;
+    }
+
+    private @ColorInt int getActionIconColor() {
+        Resources.Theme theme = getTheme();
+        TypedValue tv = new TypedValue();
+        theme.resolveAttribute(android.R.attr.textColorPrimary, tv, true);
+        Log.d(TAG, "getActionIconColor: " + tv.data);
+//        TypedValue tv2 = new TypedValue();
+//        theme.getResources(R.color.colorAccent, tv2, true);
+//        Log.d(TAG, "getActionIconColor: " + tv2.data);
+//        return tv.data;
+        return Color.BLUE;
     }
 }
